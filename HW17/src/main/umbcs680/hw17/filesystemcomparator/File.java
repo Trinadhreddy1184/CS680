734ebb6117e60ef9d8f78fca9155876f6f83df91
Trinadhreddy Seelam <trinadhreddysee@gmail.com>
2024-04-23 22:08:02 -0400

    Add files via upload

---
+++ HW17/src/main/umbcs680/hw17/filesystemcomparator/File.java
@@ -0,0 +1,34 @@
+package umbcs680.hw17.filesystemcomparator;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+public class File extends FSElement{
+
+    private String  permission;
+
+    public  File(Directory parent,String name, int size,LocalDateTime creationTime){
+        super(parent,name,size,creationTime);
+        this.permission="rw-rw-r–";
+    }
+
+    @Override
+    public boolean isDirectory(){
+        return false;
+    }
+
+
+    public void changePermission(String newPermission){
+        this.permission = newPermission;
+    }
+
+    public String getPermission(){
+        return permission;
+    }
+
+    @Override
+    public int getTotalSize(){
+        return this.getSize();
+    }
+
+}
