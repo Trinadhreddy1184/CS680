81bf519e985d3552908565087f42ee1c56e5c99e
Trinadhreddy Seelam <trinadhreddysee@gmail.com>
2024-04-25 17:54:49 -0400

    Add files via upload

---
+++ HW14/src/main/umbcs680/hw14/filesystemcomparator/Link.java
@@ -0,0 +1,28 @@
+package umbcs680.hw14.filesystemcomparator;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+public class Link extends FSElement{
+    private FSElement target;
+    public Link (Directory parent,String name,int size,LocalDateTime creationTime,FSElement target){
+        super(parent,name,size,creationTime);
+        this.target= target;
+    }
+
+    public FSElement getTarget() {
+        return target;
+    }
+
+    @Override
+    public boolean isDirectory(){
+        return false;
+    }
+    
+    @Override
+    public int getTotalSize(){
+        return 0;
+    }
+    
+}
